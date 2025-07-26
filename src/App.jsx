0169ba5b9b0813303064a35bc8787636ea977584
2300032848 <2300032848@kluniversity.in>
2025-07-26 16:48:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome To 3rd Year CICD Class S227 </h1>
+    </>
+  )
+}
+export default App
